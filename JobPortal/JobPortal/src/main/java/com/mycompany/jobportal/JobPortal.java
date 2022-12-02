41a185aeff17ea8c906a036d5df6d5b942f291ac
Md Akib Hasan <65163997+akib35@users.noreply.github.com>
2022-12-02 07:55:10 +0600

    failed project

---
+++ JobPortal/JobPortal/src/main/java/com/mycompany/jobportal/JobPortal.java
@@ -0,0 +1,10 @@
+package com.mycompany.jobportal;
+
+public class JobPortal {
+
+    public static void main(String[] args) {
+        again st = new again();
+        st.setVisible(true);
+        
+    }
+}
---
+++ JobPortal/src/login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class login {
+    private JPanel login;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
